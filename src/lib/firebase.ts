c5f7296603d03a694b2dac16a45a75121b965fde
Najmul Haque Talukder <najmulhaquetalukder41@gmail.com>
2026-05-10 07:54:38 +0000

    amar project e firebase add hocche na! manually ki korle firebase add ho

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-// Legacy configuration removed. Use @/firebase instead.
 import { initializeFirebase } from "@/firebase";
 const { firebaseApp: app, firestore: db, auth } = initializeFirebase();
 export { app, db, auth };
